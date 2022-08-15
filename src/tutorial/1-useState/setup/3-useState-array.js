e2513892ae1b989d59a8db424000c44596c05b5c
dodytrifa <dody.trifa@gmail.com>
2022-08-15 21:54:56 +0700

    usestate with array

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import { data } from '../../../data';
+import { useState } from 'react';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  //set your zero state here
+  // const [people, setPeople] = useState([]);// you can use React.useState([]) instead of useState([])
+
+  const [people, setPeople] = useState(data)
+
+  // filter returns a new array
+  const handleDelete = (id) => {
+    setPeople(people.filter(person => person.id !== id))
+  }
+
+  return (
+    <>
+    {people.map(person =>{
+        const { id, name } = person;
+        return (
+          <>
+          <p key={id} className='item'>{id}. {name}</p>
+          <button className='btn' onClick={()=> handleDelete(id)}>Clear name</button>
+          </>
+        )
+      } )
+    }
+    
+    <button className='btn' onClick={()=>setPeople([])}>Clear items</button>
+    </>
+    //to avoid re render we should input anonyme function inside onclick
+  )
 };
 
 export default UseStateArray;
