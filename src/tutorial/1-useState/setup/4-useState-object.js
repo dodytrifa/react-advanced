64be95f24287c6d48dd0a7da630be83d8a350eff
dodytrifa <dody.trifa@gmail.com>
2022-08-16 21:07:18 +0700

    deeper on usestate

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'John',
+    age: 30,
+    role: 'developer'
+  })
+
+  function changeName() {
+    setPerson({
+      ...person,
+      name: 'Jane'
+    })
+    
+  }
+
+  return (
+    <>
+      <h3>Name: {person.name}</h3>
+      <h3>Age: {person.age}</h3>
+      <h3>Role: {person.role}</h3>
+      <button className='btn' onClick={changeName}>Change Name</button>
+    </>
+  ) 
 };
 
 export default UseStateObject;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [count, setCount] = useState(0);
+
+  function addNumber () {
+    // let count = 0;
+    setCount(count + 1)
+  }
+
+  function subtractNumber () {
+    // let count = 0;
+    setCount(count - 1)
+  }
+  
+  return (
+    <>
+      <h3>Count: {count}</h3>
+      <button className='btn' onClick={addNumber}>Add Number</button>
+      <button className='btn' onClick={subtractNumber}>Substract Number</button>
+    </>
+  )
 };
 
 export default UseStateCounter;
